ca22a3b341e499021ccb03be432cc0bd66bcc2d1
Mark Schulist <mschulist2@gmail.com>
2024-10-18 22:01:16 -0500

    testing env

--- src/db_utils/create_client.ts
+++ src/db_utils/create_client.ts
@@ -4,6 +4,9 @@ import { cookies } from 'next/headers'
 export function createClient() {
   const cookieStore = cookies()
 
+  console.log(process.env.NEXT_PUBLIC_SUPABASE_URL)
+  console.log(process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
+
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
