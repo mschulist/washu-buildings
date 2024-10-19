47ca34f4a37a882c4f1216d265a15a7ad1f82981
Mark Schulist <mschulist2@gmail.com>
2024-10-18 22:38:11 -0500

    Refactor Building component to add comments section

--- src/components/Building.tsx
+++ src/components/Building.tsx
@@ -40,11 +40,16 @@ export function Building({ id }: { id: string }) {
     <div className='flex flex-col items-center min-h-screen bg-base-200 py-10'>
       <BackButton />
       <BuildingHero buildingDetails={buildingDetails} />
-      <EditCheckBox setIsEditing={setIsEditing} />
-      <BuildingEditable
-        buildingDetails={buildingDetails}
-        isEditing={isEditing}
-      />
+      <div className='grid grid-cols-3 gap-4 px-16 w-[90vw]'>
+        <div className='col-span-2 text-center p-8'>
+          <EditCheckBox setIsEditing={setIsEditing} />
+          <BuildingEditable
+            buildingDetails={buildingDetails}
+            isEditing={isEditing}
+          />
+        </div>
+        <div className='col-span-1 p-8'>Comments!</div>
+      </div>
     </div>
   )
 }
